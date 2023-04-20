ba170b0a48114b9060a9f12de0d30cb7f8dd362c
Jean-Christian Denis <contact@jcdenis.fr>
2023-04-21 00:14:50 +0200

    use namespace

--- locales/fr/main.lang.php
+++ locales/fr/main.lang.php
@@ -9,17 +9,19 @@
 # DOT NOT MODIFY THIS FILE !
 #
 
-l10n::$locales['Never']                                                                                      = 'Jamais';
-l10n::$locales['My entries']                                                                                 = 'Mes billets';
-l10n::$locales['All entries']                                                                                = 'Tous les billets';
-l10n::$locales['Email notification']                                                                         = 'Notification par e-mail';
-l10n::$locales['Notify new comments by email:']                                                              = 'Avertir des nouveaux commentaires par e-mail :';
-l10n::$locales['"%s" - New comment']                                                                         = '"%s" - Nouveau commentaire';
-l10n::$locales['Blog: %s']                                                                                   = 'Blog : %s';
-l10n::$locales['Entry: %s <%s>']                                                                             = 'Billet : %s <%s>';
-l10n::$locales['Comment by: %s <%s>']                                                                        = 'Commentaire par : %s <%s>';
-l10n::$locales['Website: %s']                                                                                = 'Site web : %s';
-l10n::$locales['Comment status: %s']                                                                         = 'Statut du commentaire : %s';
-l10n::$locales['Edit this comment: <%s>']                                                                    = 'Modifier ce commentaire : %s';
-l10n::$locales['You must log in on the backend before clicking on this link to go directly to the comment.'] = 'Vous devez vous enregistrer sur l\'interface d\'administration avant de cliquer sur ce lien pour aller directement sur ce commentaire.';
-l10n::$locales['You received a new comment on your blog:']                                                   = 'Vous avez reçu un nouveau commentaire sur votre blog :';
+use Dotclear\Helper\L10n;
+
+L10n::$locales['Never']                                                                                      = 'Jamais';
+L10n::$locales['My entries']                                                                                 = 'Mes billets';
+L10n::$locales['All entries']                                                                                = 'Tous les billets';
+L10n::$locales['Email notification']                                                                         = 'Notification par e-mail';
+L10n::$locales['Notify new comments by email:']                                                              = 'Avertir des nouveaux commentaires par e-mail :';
+L10n::$locales['"%s" - New comment']                                                                         = '"%s" - Nouveau commentaire';
+L10n::$locales['Blog: %s']                                                                                   = 'Blog : %s';
+L10n::$locales['Entry: %s <%s>']                                                                             = 'Billet : %s <%s>';
+L10n::$locales['Comment by: %s <%s>']                                                                        = 'Commentaire par : %s <%s>';
+L10n::$locales['Website: %s']                                                                                = 'Site web : %s';
+L10n::$locales['Comment status: %s']                                                                         = 'Statut du commentaire : %s';
+L10n::$locales['Edit this comment: <%s>']                                                                    = 'Modifier ce commentaire : %s';
+L10n::$locales['You must log in on the backend before clicking on this link to go directly to the comment.'] = 'Vous devez vous enregistrer sur l\'interface d\'administration avant de cliquer sur ce lien pour aller directement sur ce commentaire.';
+L10n::$locales['You received a new comment on your blog:']                                                   = 'Vous avez reçu un nouveau commentaire sur votre blog :';
